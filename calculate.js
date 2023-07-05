a1519dc90fe496ddd001ec6f95db11add992c411
Benno <107504783+BennoCrafter@users.noreply.github.com>
2023-07-05 18:26:22 +0200

    Update calculate.js added more than one aritmetic calculation

--- calculate.js
+++ calculate.js
@@ -1,42 +1,65 @@
 function calculate() {
-  var raw_nums = document.getElementById("calculation").value;
-  var result;
-  if (raw_nums.includes("+")) {
-    var nums = document.getElementById("calculation").value.split("+");
-    var num1 = nums[0];
-    var num2 = nums[1];
-    result = parseInt(num1) + parseInt(num2);
-  }
+    var calculation = document.getElementById("calculation").value;
+    var result;
+    tokens = [];
+    validOperators = ["+", "-", "*", "/"];
+    var currentToken = "";
+    
+    function little_calculate(num1, op, num2){
+        if (op === "+"){
+            return num1 + num2
+        }
+        if (op === "-"){
+            return num1 - num2
+        }
+        if (op === "*"){
+            return num1 * num2
+        }
+        if (op === "/"){
+            return num1 / num2
+        }
+    }
+    lexer(calculation);
+    function lexer(calculation){
+        for (let i = 0; i < calculation.length; i++) {
+            let char = calculation[i];
+            if (!isNaN(char)) {
+              currentToken += char;
+            } else if (validOperators.includes(char)) {
+              if (currentToken !== "") {
+                tokens.push(["INTEGER", parseInt(currentToken)]);
+                currentToken = "";
+              }
+              tokens.push(["OP", char]);
+            } else if (char.match(/[a-z]/i)) {
+              tokens.push(["VAR", char]);
+            }
+          }
+          
+          // Add the last number if it exists
+          if (currentToken !== "") {
+            tokens.push(["INTEGER", parseInt(currentToken)]);
+          }
+    }
 
-  if (raw_nums.includes("-")) {
-    var nums = document.getElementById("calculation").value.split("-");
-    var num1 = nums[0];
-    var num2 = nums[1];
-    result = parseInt(num1) - parseInt(num2);
+    while (tokens.length !== 1){
+        let num1 = tokens[0][1];
+        let op = tokens[1][1];
+        let num2 = tokens[2][1];
+        tokens = tokens.slice(3);
+        tokens.unshift(["INTEGER", little_calculate(num1, op, num2)]);    
+    }
+    result = tokens[0][1];
+    if (result === undefined) {
+      document.getElementById("calculation").classList.add("invalid");
+  } else {
+      document.getElementById("calculation").classList.remove("invalid");
+      document.getElementById("result").innerHTML = result;
   }
-  if (raw_nums.includes("*")) {
-    var nums = document.getElementById("calculation").value.split("*");
-    var num1 = nums[0];
-    var num2 = nums[1];
-    result = parseInt(num1) * parseInt(num2);
+  
   }
-  if (raw_nums.includes("/")) {
-    var nums = document.getElementById("calculation").value.split("/");
-    var num1 = nums[0];
-    var num2 = nums[1];
-    result = parseInt(num1) / parseInt(num2);
-  }
-
-  if (result === undefined) {
-    document.getElementById("calculation").classList.add("invalid");
-} else {
-    document.getElementById("calculation").classList.remove("invalid");
-    document.getElementById("result").innerHTML = result;
-}
-
-}
-document.getElementById("calculation").addEventListener("keyup", function(event) {
-  if (event.key === "Enter") {
-    document.getElementById("add-button").click();
-  }
-});
+  document.getElementById("calculation").addEventListener("keyup", function(event) {
+    if (event.key === "Enter") {
+      document.getElementById("add-button").click();
+    }
+  });
